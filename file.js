340fee91e8a68fd7eca36e45b4ce6d9b5cd1ecfa
mjsimo99 <mohashark21@gmail.com>
2022-10-20 16:44:31 +0100

    semi final calcul

--- file.js
+++ file.js
@@ -18,12 +18,21 @@ function calcul()
     if (operation === '/')
     {
         if(n2==0)
-        window.alert("error");
+        document.querySelector("#resultatno").innerHTML = "It cannot be divided by zero";
+
+
         else
         document.getElementById('resultat').value = n1/n2 ;
     }
+}
+function clearBox(elementID)
+{
+    document.getElementById('resultatno').innerHTML = "";
+    const firstNameInput1 = document.getElementById('resultat');
+    const firstNameInput2 = document.getElementById('n1');
+    const firstNameInput3 = document.getElementById('n2');
 
-
-    
-
+    firstNameInput1.value = '';
+    firstNameInput2.value = '';
+    firstNameInput3.value = '';
 }
